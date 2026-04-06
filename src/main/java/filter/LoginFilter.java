e4c67693f7f7dee9f24a5a67158da9fb139a8280
LUCAS SILVA DA ROSA <41279517883@CAMPUS.MACHADO.MCH>
2026-04-06 10:36:31 -0300

    fix: problema ligação com banco

--- src/main/java/filter/LoginFilter.java
+++ src/main/java/filter/LoginFilter.java
@@ -11,6 +11,7 @@ import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+
 @WebFilter("/*")
 public class LoginFilter implements Filter {
 	
